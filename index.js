47f37ad0c9211e181bd0bb33a4ccfbda3328a76c
Waldson Vital <waldson.vital@gmail.com>
2017-09-05 19:13:11 -0300

    estrutura

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+//requires
 const restify = require ('restify');
 const server =  restify.createServer();
+let routes = require('./routes');
 
-server.get("/", (req, res, next) => {
-    res.send("ola");
-    return next();
-});
+// plugins
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.fullResponse());
+server.use(restify.plugins.dateParser());
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.jsonp());
+server.use(restify.plugins.gzipResponse());
+server.use(restify.plugins.bodyParser());
 
+//rotas
+routes(server);
+
+//listen
 server.listen(5000, () => {
     console.log('%s listening at %s', server.name, server.url);
 });
---
+++ routes.js
@@ -0,0 +1,5 @@
+// imports routes
+
+module.exports = (server) => {
+    
+}
